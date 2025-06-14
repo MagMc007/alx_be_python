1afdde01812fc83e9a396e07dea0dd092feb8df6
MagMc007 <mergashasho7@gmail.com>
2025-06-14 14:26:41 +0300

    updated arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,16 @@
-def perform_operation(num1,num2,operation):
+def perform_operation(num1, num2, operation):
     match operation:
         case "add":
-            return f"Result: {num1 + num2}"
+            return num1 + num2
         case "subtract":
-            return f"Result: {num1 - num2}"
+            return num1 - num2
         case "multiply":
-            return f"Result: {num1 * num2}"
+            return num1 * num2
         case "divide":
             if num2 != 0:
-                return f"Result: {num1 / num2}"
+                return num1 / num2
             else:
                 return "Cannot divide by zero"
+        case _:
+            return "Invalid operation"
             
