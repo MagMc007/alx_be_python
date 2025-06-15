29c97fca25baab5bc6af226dbf391b46d51287b0
MagMc007 <mergashasho7@gmail.com>
2025-06-15 09:43:10 +0300

    updated bank_account.py and main-o.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,7 +14,7 @@ class BankAccount:
             return False
 
     def display_balance(self):
-        print(self.account_balance)
+        print(f"Current balance: ${self.account_balance}")
 
 
 
