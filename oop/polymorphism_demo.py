c026a31656daa3e5d46719e0be29ef3441116329
MagMc007 <mergashasho7@gmail.com>
2025-06-22 11:22:13 +0300

    added polymorphism_demo.py

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,23 @@
+import math
+
+
+class Shape:
+     def area(self):
+        raise NotImplementedError()
+
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.width = width
+        self.length = length
+    
+    def area(self):
+        return self.width * self.length
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
